248a054ef175212732ef4983c3225ae8d201509e
Pavel <p.tataurov92@gmail.co>
2019-10-16 15:18:08 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,163 +11,169 @@ const isDev = process.env.NODE_ENV.trim() === 'development'
 const pages = getDirectoriesBasenames(path.resolve('./src/pages'))
 
 const instances = pages.map(page => {
-  return new HtmlWebpackPlugin({
-    template: `./pages/${page}/${page}.pug`,
-    filename: `${page}.html`,
-    chunks: ['common', page]
-  })
+	return new HtmlWebpackPlugin({
+		template: `./pages/${page}/${page}.pug`,
+		filename: `${page}.html`,
+		chunks: ['bundle', page],
+		minify: !isDev
+	})
 })
 
 const entries = pages.reduce((acc, page) => {
-  acc[page] = `./pages/${page}/${page}.js`
+	acc['bundle'] = `./pages/${page}/${page}.js`
 
-  return acc
+	return acc
 }, {})
 
 const config = {
-  context: path.resolve(__dirname, 'src'),
-  entry: entries,
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js', '.scss', '.pug'],
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          output: {
-            comments: false
-          }
-        }
-      }),
-      new OptimizeCSSAssetsPlugin({
-        cssProcessor: require('cssnano'),
-        cssProcessorPluginOptions: {
-          preset: ['default', { discardComments: { removeAll: true } }]
-        }
-      })
-    ],
-    splitChunks: {
-      chunks: 'all',
-      name: 'common'
-    }
-  },
-  devServer: {
-    hot: false,
-    open: true
-  },
-  mode: isDev ? 'development' : 'production',
-  watch: isDev,
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../'
-            }
-          },
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: [require('autoprefixer')()]
-            }
-          },
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: './',
-              name: 'fonts/[name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'img/[hash].[ext]'
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              disable: isDev,
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              pngquant: {
-                quality: '65-90',
-                strip: true
-              },
-              svgo: {
-                cleanupIDs: true
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: [':src', ':href']
-          }
-        }
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: isDev
-        }
-      }
-    ]
-  },
-  plugins: [
-    ...instances,
-    new MiniCssExtractPlugin({
-      filename: isDev ? 'css/[name].css' : 'css/[name].[hash].css',
-      chunkFilename: isDev ? 'css/common.css' : 'css/common.[hash].css'
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    })
-  ].filter(Boolean)
+	context: path.resolve(__dirname, 'src'),
+	entry: entries,
+	devtool: 'inline-source-map',
+	output: {
+		filename: 'js/[name].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	resolve: {
+		extensions: ['.js', '.scss', '.pug'],
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					output: {
+						comments: false
+					}
+				}
+			}),
+			new OptimizeCSSAssetsPlugin({
+				cssProcessor: require('cssnano'),
+				cssProcessorPluginOptions: {
+					preset: ['default', { discardComments: { removeAll: true } }]
+				}
+			})
+		],
+		splitChunks: {
+			cacheGroups: {
+				styles: {
+					chunks: 'all',
+					name: 'bundle',
+					test: /\.s?css$/,
+					enforce: true
+				}
+			}
+		}
+	},
+	devServer: {
+		hot: false,
+		open: true
+	},
+	mode: isDev ? 'development' : 'production',
+	watch: isDev,
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: '../'
+						}
+					},
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: [require('autoprefixer')()]
+						}
+					},
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: './',
+							name: 'fonts/[name].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|ico)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'img/[hash].[ext]'
+						}
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							disable: isDev,
+							mozjpeg: {
+								progressive: true,
+								quality: 65
+							},
+							pngquant: {
+								quality: '65-90',
+								strip: true
+							},
+							svgo: {
+								cleanupIDs: true
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.(html)$/,
+				use: {
+					loader: 'html-loader',
+					options: {
+						attrs: [':src', ':href']
+					}
+				}
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				options: {
+					pretty: isDev
+				}
+			}
+		]
+	},
+	plugins: [
+		...instances,
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css'
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+			}
+		})
+	].filter(Boolean)
 }
 
 module.exports = config
